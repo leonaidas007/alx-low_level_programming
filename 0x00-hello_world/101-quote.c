1101677b853ea49f3c6647d34db24f3d7712b9da
Okolie Leo <xanderon75@gmail.com>
2023-07-14 12:20:34 -0700

    error message

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  *main - program begins
+  *Return: returns 1. program ends
+*/
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
