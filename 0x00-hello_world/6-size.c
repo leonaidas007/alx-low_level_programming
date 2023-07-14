432ee24989d294a07092b591511e3656e3829097
Okolie Leo <xanderon75@gmail.com>
2023-07-14 10:12:57 -0700

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  *main - program begins
+  *Return: returns 0
+ */
+int main(void)
+{
+	printf("The size of char is: %lu bytes\n", sizeof(char));
+	printf("The size of int is: %lu bytes\n", sizeof(int));
+	printf("The size of long int is: %lu bytes\n", sizeof(long int));
+	printf("The size of float is: %lu bytes\n", sizeof(float));
+	return (0);
+}
