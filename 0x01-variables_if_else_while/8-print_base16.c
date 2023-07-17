92d15cc0d50be085f1fb0eba606765a7d14c0442
Okolie Leo <xanderon75@gmail.com>
2023-07-17 02:39:53 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main- start the program
+  *Return: returns 0 and ends the program
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+			}
+	for (i = 97; i <= 102; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
