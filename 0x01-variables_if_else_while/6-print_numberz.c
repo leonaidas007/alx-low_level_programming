cad8795be33d3562aac3ebdec04b6765fb40a903
Okolie Leo <xanderon75@gmail.com>
2023-07-17 02:03:53 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Program begins
+ *Return: Returns 0
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
