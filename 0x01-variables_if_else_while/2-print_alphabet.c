55e34cff5ec45372b262ea0a5885d125098085dc
Okolie Leo <xanderon75@gmail.com>
2023-07-16 23:26:19 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main - Begin Program
+  *Return: Program return 0 and ends
+*/
+int main(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
