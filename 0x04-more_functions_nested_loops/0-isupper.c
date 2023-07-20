ea2c352e6e909aa59ccd3ad2445ad68276f46686
Okolie Leo <xanderon75@gmail.com>
2023-07-20 03:58:25 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,30 @@
+#include "main.h"
+int _isupper(int c);
+/**
+  *main - Begins the program
+  *Return: Ends program and returns 0
+*/
+int main(void)
+{
+	int _isupper(int c);
+	return (0);
+}
+/**
+  *_isupper()- Checks for Uppercase
+  *Return: returns 1 if in upperase, returns 0 if in lowercase
+*/
+int _isupper(int c)
+{
+	int i;
+	c = 65;
+
+	if (c <= 90)
+	{
+		i = 1;
+	}
+	else
+	{
+		i = 0;
+	}
+	return (i);
+}
