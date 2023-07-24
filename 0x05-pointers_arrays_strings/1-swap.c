ed5db769cd14958015ff63a935ddfe734aae0e7f
Okolie Leo <xanderon75@gmail.com>
2023-07-24 16:30:48 -0700

    Dont swap horses

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *swap_int - funtions swaps the value of two variables
+  *@*a, @*b - points to variables of int type
+  *Returns : nothing
+*/
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
