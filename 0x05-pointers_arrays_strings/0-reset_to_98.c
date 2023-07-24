752375cc6bf86b0dfb46918b45ef07edb75eb003
Okolie Leo <xanderon75@gmail.com>
2023-07-24 16:17:15 -0700

    Battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
-  * reset_to_98 - resets the value of variable pointed to
-  * @int *n -  points to an int variable
-  * Returns : nothing
+  *reset_to_98 - resets the value of variable pointed to
+  *@*n   points to an int variable
+  *Returns : nothing
 */
 void reset_to_98(int *n)
 {
