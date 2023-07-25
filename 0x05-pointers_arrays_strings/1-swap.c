f55e15240ebf60732916cf76781955b1be9c0e44
Okolie Leo <xanderon75@gmail.com>
2023-07-25 04:48:35 -0700

    stringlength

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 /**
   *swap_int - funtions swaps the value of two variables
-  *@*a, @*b - points to variables of int type
+  *@a : integer to swap
+  *@b :  integer to swap
   *Returns : nothing
 */
 void swap_int(int *a, int *b)
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+int _strlen(char *s)
+{
+	int length = 0 ;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
